f006a9b4ea7e275dc1a5f44707513df69d3caf35
Egor Solodovnikov <egor.solo1@yandex.ru>
2022-10-30 00:45:08 +0200

    initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+const Home: NextPage = () => {
+  return (
+    <div className="h-screen w-full bg-gray-200">
+      <div className="bg-white"></div>
+    </div>
+  );
+};
+
+export default Home;
