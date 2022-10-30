e65f9ff716d110c5888b3e8641a38caf416acfef
Egor Solodovnikov <egor.solo1@yandex.ru>
2022-10-30 04:22:41 +0100

    fixed sharing functionality, fixed pagination offset

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { type NextPage } from "next";
-import { useQuery, useQueryClient } from "react-query";
+import { useQuery } from "react-query";
 import Loader from "../components/loader";
 import getNewsArticles from "../utils";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Dialog from "@mui/material/Dialog";
 
+//type for the article object
 interface InewsArticle {
   title: string;
   url: string;
@@ -17,50 +18,84 @@ const DEFAULT_IMAGE =
   "https://fashionunited.info/global-assets/img/default/fu-default_1200x630_black-favicon.jpg";
 
 const Home: NextPage = () => {
+  //router initialization and query retreival, for the sharing functionality
   const router = useRouter();
+  const { query } = router;
 
+  //article array and offset state
   const [newsArticles, setNewsArticles] = useState<Array<InewsArticle>>([]);
   const [offset, setOffset] = useState(0);
 
-  const [dialogOpen, setDialogOpen] = useState({
-    id: 0,
-    isOpen: false,
-  });
-
+  //initial batch fetch via react query
   const { data, isLoading, isError } = useQuery(
     ["newsArticles"],
     getNewsArticles
   );
 
+  //redirect in case fetch fails
   isError && router.push("/404");
 
+  //effect for making sure we show the dialog with the linked news article in case there's a query parameter right away
   useEffect(() => {
     data && setNewsArticles(data.fashionunitedNlNewsArticles);
+
+    const fetchLinkedNew = async () => {
+      const res = await getNewsArticles({}, Number(query.id), 1);
+      console.log(res);
+      setNewsArticles((prevNewsArticles) => [
+        ...prevNewsArticles,
+        ...res.fashionunitedNlNewsArticles,
+      ]);
+    };
+
+    if (query.id) {
+      fetchLinkedNew();
+    }
   }, [data]);
 
+  //loadMore function that is run when the button is clicked
   const loadMore = async () => {
-    setOffset((prevOffset) => (prevOffset += 26));
-    const newBatch = await getNewsArticles({}, offset + 26);
+    setOffset((prevOffset) => (prevOffset += 13));
+
+    //fetching step
+    if (newsArticles.length % 13 != 0) {
+      const formattedArticles = newsArticles.slice(0, newsArticles.length - 1);
+      setNewsArticles(formattedArticles);
+    }
+
+    //state setting step
+    const newBatch = await getNewsArticles({}, offset + 13);
     setNewsArticles((prevArticles) => [
       ...prevArticles,
       ...newBatch.fashionunitedNlNewsArticles,
     ]);
   };
 
-  const toggleDialog = (_id: number, isOpen: boolean) => {
-    if (isOpen) {
-      setDialogOpen({
-        id: _id,
-        isOpen: true,
-      });
+  //function for showing and hiding a dialog, the state of it is tied to the query string in url
+  const toggleDialog = (isOpen: boolean, _id?: number, _title?: string) => {
+    if (isOpen && _id != undefined && _title) {
+      router.push(
+        {
+          query: { id: _id, title: _title },
+        },
+        undefined,
+        { shallow: true }
+      );
     } else {
-      setDialogOpen({ id: 0, isOpen: false });
+      router.push(
+        {
+          query: {},
+        },
+        undefined,
+        { shallow: true }
+      );
     }
   };
 
   return (
     <div className="grid h-full w-full items-center justify-items-center bg-gray-200 p-12">
       <div className="pb-12 text-4xl">Fashion News</div>
+
       <div className="grid h-full w-full max-w-7xl grid-cols-5 justify-items-center gap-4">
         {isLoading ? (
           <Loader />
@@ -69,7 +104,7 @@ const Home: NextPage = () => {
           newsArticles.map((a: InewsArticle, index: number) => (
             <>
               <div
-                onClick={() => toggleDialog(index, true)}
+                onClick={() => toggleDialog(true, index, a.title)}
                 className={`h-88 w-full rounded bg-white font-semibold shadow ${
                   index == 0 || index % 13 == 0
                     ? "h-8xl col-span-2 row-span-2 text-3xl"
@@ -105,36 +140,36 @@ const Home: NextPage = () => {
                   </div>
                 </div>
               </div>
-              {dialogOpen ? (
-                <Dialog
-                  open={dialogOpen.id == index && dialogOpen.isOpen}
-                  onClose={() => toggleDialog(0, false)}
-                >
-                  <div className="h-88 h-8xl col-span-2 row-span-2 w-full rounded bg-white text-3xl font-semibold shadow">
-                    <div className="rows-[4fr_3fr_1fr] grid h-full w-full">
-                      <div className="h-88">
-                        <img
-                          src={a.imageUrl || DEFAULT_IMAGE}
-                          className="h-full w-full rounded-t object-cover"
-                        />
-                      </div>
-                      <h2 className="px-3 pt-3">{a.title}</h2>
-                      <div className="p-3 text-base font-normal opacity-60">
-                        {a.description}
-                      </div>
-                      <div className="self-end px-3 py-4">
-                        <a href={a.url} className="text-base underline ">
-                          Read more
-                        </a>
-                      </div>
+
+              <Dialog
+                open={query.title == a.title}
+                onClose={() => toggleDialog(false)}
+              >
+                <div className="h-88 h-8xl col-span-2 row-span-2 w-full rounded bg-white text-3xl font-semibold shadow">
+                  <div className="rows-[4fr_3fr_1fr] grid h-full w-full">
+                    <div className="h-88">
+                      <img
+                        src={a.imageUrl || DEFAULT_IMAGE}
+                        className="h-full w-full rounded-t object-cover"
+                      />
+                    </div>
+                    <h2 className="px-3 pt-3">{a.title}</h2>
+                    <div className="p-3 text-base font-normal opacity-60">
+                      {a.description}
+                    </div>
+                    <div className="self-end px-3 py-4">
+                      <a href={a.url} className="text-base underline ">
+                        Read more
+                      </a>
                     </div>
                   </div>
-                </Dialog>
-              ) : null}
+                </div>
+              </Dialog>
             </>
           ))
         )}
       </div>
+
       <div className="mt-16 grid justify-items-center">
         <button
           type="button"
